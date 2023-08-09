645a8804ea90d7333e421e0f3a9bcb2f6f63ceab
onyournn <saritam64@nu.ac.th>
2023-08-09 17:12:49 +0700

    install express

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+let express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/status', function (req,res){
+
+    res.send('Hello nodejs server i am noonnaaaa')
+
+})
+
+
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.name)
+    res.send('sey hello with ' + req.params.name)
+})
+
+let port = 8081
+
+app.listen(port, function(){
+
+    console.log('server running on ' + port)
+
+})
