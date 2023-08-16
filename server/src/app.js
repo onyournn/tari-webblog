65cdd831b07a41058a8ad22d00d5aa0cc0fbb61a
onyournn <saritam64@nu.ac.th>
2023-08-16 23:37:47 +0700

    finish page72

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,20 @@
 let express = require('express')
-
+let bodyParser = require('body-parser')
 const app = express()
-
- 
-
-app.get('/status', function (req,res){
-
-    res.send('Hello nodejs server i am noonnaaaa')
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+require('./routes')(app)
+app.get('/status', function (req, res ){
+ res.send('Hello nodejs server')
 })
-
-
-app.get('/hello/:name', function (req,res) {
-    console.log('hello - ' + req.params.name)
-    res.send('sey hello with ' + req.params.name)
+app.get('/hello/:person', function (req,res) {
+ console.log('hello - ' + req.params.person)
+ res.send('sey hello with ' + req.params.person)
 })
-
+app.post('/hello', function (req, res) {
+ res.send('OK you post - ' + req.body.name)
+})
 let port = 8081
-
-app.listen(port, function(){
-
-    console.log('server running on ' + port)
-
+app.listen(port, function () {
+ console.log('server running on ' + port)
 })
