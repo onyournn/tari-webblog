ac0c76b7508710c073e16ca37f9f892a6656071d
onyournn <saritam64@nu.ac.th>
2023-08-17 00:01:32 +0700

    finish page82

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,57 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
+
 const app = express()
+
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 require('./routes')(app)
-app.get('/status', function (req, res ){
- res.send('Hello nodejs server')
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
 })
-app.get('/hello/:person', function (req,res) {
- console.log('hello - ' + req.params.person)
- res.send('sey hello with ' + req.params.person)
+
+app.get('/hello/:person', function (req, res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
 })
-app.post('/hello', function (req, res) {
- res.send('OK you post - ' + req.body.name)
+
+// get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
 })
-let port = 8081
-app.listen(port, function () {
- console.log('server running on ' + port)
+
+// get all user
+app.get('/users', function (req, res) {
+    res.send('เรียกใช้ข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+
+
+// edit user
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + JSON.stringify(req.body))
+})
+
+// delete user
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทําการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
+        JSON.stringify(req.body))
+
+})
+// create user
+app.post('/user', function (req, res) {
+    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+})
+
+let port = process.env.PORT || config.port
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
